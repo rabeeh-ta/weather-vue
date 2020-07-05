2babb94d3a4eda5e9df92f9e2b8dcfa10e7e3d17
Rabeeh T A <rabeehchemnad@gmail.com>
2020-07-05 20:15:08 +0530

    � weather based on your location added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
+    "
+  >
     <main>
       <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
+        <input
+          type="text"
+          class="search-bar"
           placeholder="Search..."
           v-model="query"
           @keypress="fetchWeather"
         />
+        <i @click="getLocation" class="large material-icons">location_on</i>
       </div>
-
-      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+      <!-- for error-->
+      <div v-if="weather.message" class="err-message">{{ weather.message }}</div>
+      <div class="weather-wrap" v-if="weather.main">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
           <div class="date">{{ dateBuilder() }}</div>
@@ -28,31 +35,85 @@
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      api_key: 'f6a2c317f97367bfe8fa3e5fc517e80d',
-      url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: '',
+      api_key: "c96d98df1a581d07698cd0589b0b3de2",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      lat: null,
+      long: null,
       weather: {}
-    }
+    };
   },
   methods: {
-    fetchWeather (e) {
+    fetchWeather(e) {
       if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`
+        )
           .then(res => {
             return res.json();
-          }).then(this.setResults);
+          })
+          .then(data => {
+            this.weather = data;
+          })
+          .catch(err => {
+            alert(err.message);
+          });
       }
     },
-    setResults (results) {
-      this.weather = results;
+    fecthWithLatLong() {
+      if (this.lat != null && this.long != null) {
+        fetch(
+          `${this.url_base}weather?lat=${this.lat}&lon=${this.long}&appid=${this.api_key}&units=metric`
+        )
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            this.weather = data;
+          });
+      }
     },
-    dateBuilder () {
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.lat = position.coords.latitude;
+          this.long = position.coords.longitude;
+
+          this.fecthWithLatLong();
+        },
+        error => {
+          alert(error.message);
+        }
+      );
+    },
+    dateBuilder() {
       let d = new Date();
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
 
       let day = days[d.getDay()];
       let date = d.getDate();
@@ -62,7 +123,7 @@ export default {
       return `${day} ${date} ${month} ${year}`;
     }
   }
-}
+};
 </script>
 
 <style>
@@ -73,42 +134,52 @@ export default {
 }
 
 body {
-  font-family: 'montserrat', sans-serif;
+  font-family: "montserrat", sans-serif;
 }
 
 #app {
-  background-image: url('./assets/cold-bg.jpg');
-  background-size: cover;
-  background-position: bottom;
+  background-image: url("./assets/cold-bg.jpg");
+  background-size: contain;
+  background-position: center;
   transition: 0.4s;
 }
 
 #app.warm {
-  background-image: url('./assets/warm-bg.jpg');
+  background-image: url("./assets/warm-bg.jpg");
 }
 
 main {
   min-height: 100vh;
   padding: 25px;
 
-  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
 }
 
 .search-box {
   width: 100%;
   margin-bottom: 30px;
 }
+.search-box i {
+  position: relative;
+  float: right;
+  font-size: 50px;
+  color: white;
+  cursor: pointer;
+}
 
 .search-box .search-bar {
-  display: block;
-  width: 100%;
+  width: 80%;
   padding: 15px;
-  
+
   color: #313131;
   font-size: 20px;
 
   appearance: none;
-  border:none;
+  border: none;
   outline: none;
   background: none;
 
@@ -125,7 +196,14 @@ main {
 }
 
 .location-box .location {
-  color: #FFF;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.err-message {
+  color: #fff;
   font-size: 32px;
   font-weight: 500;
   text-align: center;
@@ -133,7 +211,7 @@ main {
 }
 
 .location-box .date {
-  color: #FFF;
+  color: #fff;
   font-size: 20px;
   font-weight: 300;
   font-style: italic;
@@ -147,12 +225,12 @@ main {
 .weather-box .temp {
   display: inline-block;
   padding: 10px 25px;
-  color: #FFF;
+  color: #fff;
   font-size: 102px;
   font-weight: 900;
 
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color:rgba(255, 255, 255, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
 
@@ -160,10 +238,16 @@ main {
 }
 
 .weather-box .weather {
-  color: #FFF;
+  color: #fff;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+@media (min-width: 240px) and (max-width: 720px) {
+  #app {
+    background-size: cover;
+  }
 }
 </style>
